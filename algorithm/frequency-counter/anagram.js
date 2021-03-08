9534c9548f8d40c8b0b3519dca9dcf6f295c5cf6
anujm4467 <anujm4467@gmail.com>
2021-03-08 22:44:17 +0530

    Added

--- algorithm/frequency-counter/anagram.js
+++ algorithm/frequency-counter/anagram.js
@@ -5,7 +5,17 @@
 
 const anagram = (s1, s2) => {
     if (s1.length !== s2.length) return false;
-
+    let wordCount = {};
+    for (const item of s1) {
+        wordCount[item] = (wordCount[item] || 0) + 1;
+    }
+    for (const item of s2) {
+        if (!wordCount[item]) {
+            return false;
+        }
+        --wordCount[item];
+    }
+    return true;
 };
 
-anagram('abcd', 'dbca');
+console.log(anagram('abcdaf', 'dbcaaa'));
