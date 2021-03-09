3981dc7895882e23b39ae3551448e237d99794c2
goutham001 <goutham015@gmail.com>
2021-03-09 13:11:40 +0530

    Added

--- algorithm/array/shiftZero.js
+++ algorithm/array/shiftZero.js
@@ -9,26 +9,18 @@
 
 
 const pushZerosToEnd = (arr, n) => {
-    let count = 0;  // Count of non-zero elements 
-    // Traverse the array. If element encountered is non- 
-    // zero, then replace the element at index 'count'  
-    // with this element 
-    for (let i = 0; i < arr.length - 1; i++) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
         if (arr[i] != 0) {
             arr[count++] = arr[i];
         }
     }
-    // here count is  
-    // incremented 
 
-    // Now all non-zero elements have been shifted to  
-    // front and  'count' is set as index of first 0.  
-    // Make all elements 0 from count to end. 
-    while (count < n) {
+    while (count < arr.length) {
         arr[count++] = 0;
     }
     return arr;
 }
 
 
-console.log(pushZerosToEnd([1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0]))
+console.log(pushZerosToEnd([1, 9, 0, 4, 12, 0]))
