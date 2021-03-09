647297d9b000eef2de0c7960f263147995f7f1e6
anujm4467 <anujm4467@gmail.com>
2021-03-09 18:54:21 +0530

    Added

--- algorithm/array/shiftZero.js
+++ algorithm/array/shiftZero.js
@@ -8,16 +8,16 @@
 */
 
 
-const pushZerosToEnd = (arr, n) => {
+const pushZerosToEnd = (arr) => {
     let count = 0;
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] != 0) {
-            arr[count++] = arr[i];
+        if (arr[i] !== 0) {
+            arr[count] = arr[i];
+            count++;
         }
     }
-
-    while (count < arr.length) {
-        arr[count++] = 0;
+    for (let i = count; i < arr.length; i++) {
+        arr[count] = 0;
     }
     return arr;
 }
