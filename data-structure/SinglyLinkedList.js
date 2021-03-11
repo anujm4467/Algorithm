a8a1cec5938b48f17613f70ea4daa97653e61cb5
anujm4467 <anujm4467@gmail.com>
2021-03-11 20:01:44 +0530

    Added

--- data-structure/SinglyLinkedList.js
+++ data-structure/SinglyLinkedList.js
@@ -51,6 +51,86 @@ class SinglyLinkedList {
         }
         return current;
     }
+
+    shifting() {
+        if (!this.head) return null;
+        let current = this.head.next;
+        this.head = current;
+        this.length--;
+    }
+
+    unshift(value) {
+        const newHead = new Node(value);
+        if (!this.head) {
+            this.head = newHead;
+            this.tail = this.head;
+        };
+        newHead.next = this.head;
+        this.head = newHead;
+        this.length++;
+    }
+
+    get(value) {
+        if (!this.head) return null;
+        let current = this.head;
+        let count = 0;
+        while (current.next) {
+            if (current.value === value) {
+                return count;
+            }
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    getValueByIndex(index) {
+        if (index < 0 || index >= this.length) return null;
+        let current = this.head;
+        let count = 0;
+        while (count > index) {
+            current = current.next;
+            count++;
+        }
+        return current;
+    }
+
+    update(value, index) {
+        let node = this.getValueByIndex(index);
+        if (node) {
+            node.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    insert(value, index) {
+        if (index < 0 || index >= this.length) return null;
+        if (index == 0) return this.unshift(value);
+        if (index === this.length - 1) this.push(value);
+        let newNode = new Node(value);
+        let prvNode = this.getValueByIndex(index - 1);
+        let temp = prvNode.next;
+        prvNode.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) return null;
+        if (index == 0) return this.shifting(value);
+        if (index === this.length - 1) this.pop();
+        let prvNode = this.getValueByIndex(index - 1);
+        let removed = prvNode.next;
+        prvNode.next = removed.next;
+        this.length--;
+        return removed;
+    }
+
+    revers() {
+
+    }
 }
 
 const list = new SinglyLinkedList();
@@ -59,5 +139,6 @@ list.push('b');
 list.push('c');
 list.push('d');
 list.push('e');
-list.pop();
-list.traverse()
+list.traverse();
+console.log(list.get('k'));
+
